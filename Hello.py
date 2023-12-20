987f44eed7184bf1d60c157d1b63689fe8521e2a
markusedgar <140502411+markusedgar@users.noreply.github.com>
2023-12-20 21:37:55 +0000

    add LLM demo to main page

--- Hello.py
+++ Hello.py
@@ -1,51 +1,29 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import streamlit as st
-from streamlit.logger import get_logger
+from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
 
-LOGGER = get_logger(__name__)
+st.title("🦜🔗 Langchain - Blog Outline Generator App")
 
-
-def run():
-    st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
-    )
-
-    st.write("# Welcome to Streamlit! 👋")
-
-    st.sidebar.success("Select a demo above.")
-
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
+openai_api_key = st.sidebar.text_input("OpenAI API Key", type="password")
 
 
-if __name__ == "__main__":
-    run()
+def blog_outline(topic):
+    # Instantiate LLM model
+    llm = OpenAI(model_name="text-davinci-003", openai_api_key=openai_api_key)
+    # Prompt
+    template = "As an experienced data scientist and technical writer, generate an outline for a blog about {topic}."
+    prompt = PromptTemplate(input_variables=["topic"], template=template)
+    prompt_query = prompt.format(topic=topic)
+    # Run LLM model
+    response = llm(prompt_query)
+    # Print results
+    return st.info(response)
+
+
+with st.form("myform"):
+    topic_text = st.text_input("Enter prompt:", "")
+    submitted = st.form_submit_button("Submit")
+    if not openai_api_key:
+        st.info("Please add your OpenAI API key to continue.")
+    elif submitted:
+        blog_outline(topic_text)
